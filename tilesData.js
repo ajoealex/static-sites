57fe7d72a652dfde0dd6399a3ef69744cb3f7def
Ajoe Alex <ajoealex316@gmail.com>
2025-10-29 11:39:41 +0530

    update

--- tilesData.js
+++ tilesData.js
@@ -1,5 +1,6 @@
 // tilesData.js
 
 const tilesData = [
-    { name: "Web table", link: "https://ajoealex.github.io/static-sites/apps/webtable/employee_web_table_single_file_html.html"}    
+    { name: "Web table", link: "https://ajoealex.github.io/static-sites/apps/webtable/employee_web_table_single_file_html.html" },
+    { name: "Touch Interaction Test", link: "https://ajoealex.github.io/static-sites/apps/touch_interactions/shopping_cart.html" }
 ];
